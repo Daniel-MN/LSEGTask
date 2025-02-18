94e53930802bd2def1c45444b30e9bfff78ff1fa
Musuroi Daniel <musuroidaniel01@gmail.com>
2025-02-18 08:16:19 +0200

    Added resources and create the LogsMonitorApp.

--- src/main/java/org/task/lseg/LSEGTask.java
+++ src/main/java/org/task/lseg/LSEGTask.java
@@ -1,7 +1,17 @@
-package org.task.lseg;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package org.task.lseg;
+
 public class LSEGTask {
     public static void main(String[] args) {
+        String logFilePath = System.getProperty("logFilePath");
+        String outputFilePath = System.getProperty("outputFilePath");
+        String alertWarningThreshold = System.getProperty("alertWarningThreshold");
+        String alertErrorThreshold = System.getProperty("alertErrorThreshold");
 
+        if (logFilePath == null || outputFilePath == null || alertWarningThreshold == null || alertErrorThreshold == null) {
+             throw new RuntimeException("Please provide the log file path, output file path, alert warning threshold and alert error threshold");
+        }
+
+        LogsMonitorApp logsMonitorApp = new LogsMonitorApp();
+        logsMonitorApp.start();
     }
 }
---
+++ src/main/java/org/task/lseg/LogsMonitorApp.java
@@ -0,0 +1,8 @@
+package org.task.lseg;
+
+public class LogsMonitorApp {
+    public void start() {
+        // read the csv and process the logs
+        // write the logs to the output file
+    }
+}
