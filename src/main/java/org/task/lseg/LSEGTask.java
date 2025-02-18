d5b5de2a8d189a8b6a87bb98b361f412db833483
Musuroi Daniel <musuroidaniel01@gmail.com>
2025-02-18 08:12:41 +0200

    Created maven project

---
+++ src/main/java/org/task/lseg/LSEGTask.java
@@ -0,0 +1,7 @@
+package org.task.lseg;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class LSEGTask {
+    public static void main(String[] args) {
+
+    }
+}
